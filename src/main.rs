67d27b492fce78c139cc5e7e422e35a806ba44c1
David Jackson <dwjackson@noreply.users.github.com>
2025-09-22 18:58:30 -0400

    Add option to copy URL to clipboard

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use arboard::{Clipboard, LinuxClipboardKind, SetExtLinux};
+use clap::Parser;
 use feedme::find_feed_url;
-use std::env;
 use std::process;
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("USAGE: feedme [URL]");
-        process::exit(1);
-    }
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long, help = "Copy to clipboard")]
+    copy: bool,
 
-    let url = &args[1];
+    url: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let url = &args.url;
+
     let body = match ureq::get(url).call() {
         Ok(mut x) => match x.body_mut().read_to_string() {
             Ok(s) => s,
@@ -19,6 +26,17 @@ fn main() {
     };
     if let Some(feed_url) = find_feed_url(url, &body) {
         println!("{}", feed_url);
+
+        if args.copy {
+            println!("Copied to clipboard");
+            let mut clipboard = Clipboard::new().unwrap();
+            clipboard
+                .set()
+                .clipboard(LinuxClipboardKind::Clipboard)
+                .wait()
+                .text(feed_url)
+                .unwrap();
+        }
     } else {
         process::exit(1);
     }
